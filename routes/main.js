e5bbc78772d6e5c614525d1cd3b045a5b90db942
Theo Roweth <theoroweth@gmail.com>
2025-11-05 15:47:17 +0000

    Finished first part of lab

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,27 @@
+// Define our data
+var shopData = {shopName: "The Thirsty Student", 
+                productCategories:["Beer", "Wine", "Soft Drinks", "Hot Drinks"]}
+
 // Create a new router
 const express = require("express");
 const router = express.Router();
 
 // Handle the main routes
+
+
 router.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", shopData)
 }); 
 
 router.get("/about", (req, res) => {
-    res.render("about.ejs")
+    res.render("about.ejs", shopData)
 });
+
+router.get("/search", (req, res) => {
+    res.render("search.ejs", shopData)
+});
+
+
 
 // Export the router object so index.js can access it
 module.exports = router;
